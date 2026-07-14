dbd1144d43214a5a116541d1ab2e0ced313676b9
jesusmjunior2021 <68130668+jesusmjunior2021@users.noreply.github.com>
2026-07-13 22:07:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ import datetime
 import requests
 import pandas as pd
 import streamlit as st
-import plotly.express as px
 
 # ==========================================================================
 # CONFIG
